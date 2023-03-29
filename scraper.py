67aed8709ec8d7bc94b1375886ce9e53c902f2e0
JanPaluch <226092@student.uek.krakow.pl>
2023-03-29 16:24:15 +0200

    2023-03-29 KW-fix

--- scraper.py
+++ scraper.py
@@ -1,5 +1,7 @@
 import requests
+from bs4 import BeautifulSoup
 product_code = "62290435"
 url = f"https://www.ceneo.pl/{product_code}#tab=reviews"
 response = requests.get(url)
-print(response.status_code)
+page_dom = BeautifulSoup(response.text, "html.parser")
+print(page_dom.prettify())
